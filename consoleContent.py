22b96400d67373077381a58269ce16dd26981239
leejongsung <time8970@naver.com>
2019-04-18 21:14:54 +0900

    test present line contetn

--- consoleContent.py
+++ consoleContent.py
@@ -1,7 +1,7 @@
 import os
 
-#contents = os.popen("git diff HEAD HEAD~1 --word-diff=plain").readlines()
-contents = os.popen("git diff HEAD HEAD~1 --word-diff").readlines()
+#contents = os.popen("git diff HEAD~1 HEAD --word-diff=plain").readlines()
+contents = os.popen("git diff HEAD~1 HEAD --word-diff=plain").readlines()
 
 for line in contents:
     print(line.strip())
