766866b76e27a91eab8224345d8372c82295e9a8
leejongsung <time8970@naver.com>
2019-04-19 10:24:19 +0900

    한글 확인

--- consoleContent.py
+++ consoleContent.py
@@ -11,24 +11,45 @@ import re
 text!
 6. hello world!
 """
-messageList = list()
 
-contents = os.popen("git diff HEAD~1 HEAD~2 --word-diff=plain").readlines()
-contents = [content.strip() for content in contents] # remove /n
-logs = os.popen("git log").readlines()
-logs = [log.strip() for log in logs] # remove /n
+#contents = os.popen("git diff HEAD~1 HEAD --word-diff=plain").readlines()
+#contents = [content.strip() for content in contents] # remove /n
 
-# for i, line in enumerate(logs):
-#     if(i == 4 or (i - 4) % 6 == 0):
-#         messageList.append(line)
-#         print("line {}: {}".format(i, line))
+class GitLog:
+    def __init__(self, fileName):
+        self.fileName = fileName
+        self.messageList = list()
 
-add_pattern = re.compile("{\+(.*)\+}")
-sub_pattern = re.compile("\[-(.*)-\]")
+    def makeMessageList(self):
+        logs = os.popen("git log").readlines()
+        logs = [log.strip() for log in logs] # remove \n
 
-for i, content in enumerate(contents):
-    if(re.search(add_pattern, content)):
-        start, end = re.search(add_pattern, content).span()
-        print(content[start:end])
+        for i, line in enumerate(logs):
+            if(i == 4 or (i - 4) % 6 == 0):
+                self.messageList.append(line)
+                print("line {}: {}".format(i, line))
+
+    def displayChange(self):
+        for i, message in enumerate(self.messageList):
+            contents = os.popen("git diff --word-diff=plain HEAD~{i} HEAD {fileName}".format(i=i, fileName=self.fileName)).readlines()
+            contents = [content.strip() for content in contents]
+            print("commit number {}  : ".format(i))
+            print("message: {}".format(message))
+            for content in contents:
+                print(content)
+            print("\n")
+
+if __name__ == "__main__":
+    gitlog = GitLog("hello.txt")
+    gitlog.makeMessageList()
+    gitlog.displayChange()
+
+#add_pattern = re.compile("{\+(.*)\+}")
+#sub_pattern = re.compile("\[-(.*)-\]")
+
+# for i, content in enumerate(contents):
+#     if(re.search(add_pattern, content)):
+#         start, end = re.search(add_pattern, content).span()
+#         print(content[start:end])
     
     
